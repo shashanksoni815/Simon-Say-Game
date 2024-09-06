7533ce8cd4e71f6ebcb2ac30c283b7ade9b6473a
Shashank Soni <shashanksoni815@gmail.com>
2024-09-06 11:40:08 +0530

    game

---
+++ app.js
@@ -0,0 +1,48 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2"); 
+
+document.addEventListener("keypress", function () {
+    if (started == false) {
+        console.log("game is started");
+        started == true;
+
+        levelUp();
+    }
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 1000);
+}
+
+function levelUp() {
+    level++;
+    h2.innerText= `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns(randIdx);
+    let randBin = document.querySelector(`.${randColor}`)
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBin);
+    btnFlash(randBin);
+}
+
+function btnPress() {
+    let btn = this;
+    btnFlash(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
